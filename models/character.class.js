324c9d377b0e07e64309341c6b4a08bd78396a35
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 16:37:02 +0100

    deleted currenImage

--- models/character.class.js
+++ models/character.class.js
@@ -11,8 +11,6 @@ class Character extends MovableObject {
             'img/2_character_pepe/2_walk/W-26.png'
     ];
 
-    currentImage = 0;
-
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
