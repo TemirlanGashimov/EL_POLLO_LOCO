4f833f9de496c282dd6edb1133696ec00bcd0d45
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 15:40:20 +0100

    added loadOmages arr

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,15 +1,27 @@
 class MovableObject {
   x = 120;
   y = 280;
+  img;
   height = 150; // höhe von objecten wie Charakter Chicken
   width = 100;  // breite von objecten wie Character und Chicken
-  img;
-
+  imageCache = {};
 
 //loadImage('img/test.png);
   loadImage(path){
     this.img = new Image(); //this.img = document.getElementById('image') <img id="image" src>
     this.img.src = path;
+  }
+
+  //**
+//    * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+//  
+
+  loadImages(arr){
+    arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = path;
+    });
   }
 
   moveRight() {
