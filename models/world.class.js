65b55f183c2aee51e77252d29a575bf480a6ef7e
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 13:20:30 +0100

    added clouds, added forEach

--- models/world.class.js
+++ models/world.class.js
@@ -5,14 +5,36 @@ class World{
         new Chicken(),
         new Chicken(),
     ];
+    clouds = [
+        new Cloud()
+    ];
+    canvas;
+    ctx;
 
-
-
-
-
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
 
 
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemy => {
+             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);    
+        });
+
+         this.clouds.forEach(clouds => {
+             this.ctx.drawImage(clouds.img, clouds.x, clouds.y, clouds.width, clouds.height);    
+        });
+
+
+        // Draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        });
     }
 }
