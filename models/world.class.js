0bee84d808e30cfa71a816bfedb642db4e65d285
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 14:13:31 +0100

    added new 3 backgroundImages, and chaged the draw function

--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,11 @@ class World{
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0), // 0,100 positionierung von hintergrundBild
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+    ];
     canvas;
     ctx;
 
@@ -17,24 +22,30 @@ class World{
         this.draw();
     }
 
-
-    draw(){
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);    
-        });
-
-         this.clouds.forEach(clouds => {
-             this.ctx.drawImage(clouds.img, clouds.x, clouds.y, clouds.width, clouds.height);    
-        });
-
-
         // Draw() wird immer wieder aufgerufen
+    draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // canvas wird gelöscht
+
+        this.addObjectsToMap(this.backgroundObjects); // das hintergrund soll als erste dargestellt werden damit die objecte auf der hintergrund zu sehen/befinden sind
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        
+
         let self = this;
         requestAnimationFrame(function(){
             self.draw();
         });
+    }
+
+    addObjectsToMap(objects){
+        objects.forEach(o => {
+            this.addToMap(o)
+        });
+
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
