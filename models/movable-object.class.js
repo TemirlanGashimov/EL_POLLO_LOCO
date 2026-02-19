13b650e06b48293d575a1b6a814ce2e1ce3fc2df
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 13:19:50 +0100

    adds and changes

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,10 +1,16 @@
 class MovableObject {
   x = 120;
-  y = 400;
+  y = 250;
+  height = 150;
+  width = 100;
   img;
 
 
-  
+//loadImage('img/test.png);
+  loadImage(path){
+    this.img = new Image(); //this.img = document.getElementById('image') <img id="image" src>
+    this.img.src = path;
+  }
 
   moveRight() {
     console.log("Moving right");
