22abb3719c53457b27afec33798da89115b6ecf1
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 14:50:56 +0100

    added y , height and width for chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject{
-  y = 300;
+  y = 370;      // platzierung auf y Achse // sobald die höche sich änder muss man auch die y Achse ändern damit die auf eine ebene sind +-
+  height = 50; // höche von unsere chicken wie hoch/groß die sind 
+  width = 85; //Breite von unseren chicken 
  
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
