faca515f0a1e0d66d6e99aef036b9fd76a0fe3b9
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 14:14:04 +0100

    nothing

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -6,6 +6,7 @@ height = 250;
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png');
     
-    this.x = 200 + Math.random() * 500; //Zahl zwischen 200 und 700     
+    this.x = 200 + Math.random() * 500; //Zahl zwischen 200 und 700 
+    
     }
 }
