ef24400e6dce86cad2493cc10ea79727a3111b39
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-19 14:32:44 +0100

    change height from 400 to 480

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,7 @@
 class BackgroundObject extends MovableObject{
 
     width = 720; //feste breite für hintergrund
-    height = 400; // feste höhe für hintergrund
+    height = 480; // feste höhe für hintergrund
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
         this.x = x;
