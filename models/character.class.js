31fe7ffabc9461a20778aef94fe40fc718a9e71a
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-20 11:57:43 +0100

    Added character movement speed forward and backward using left and right button inputs.

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject {
   height = 300;
   y = 140;
+  speed = 10;
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
@@ -19,16 +20,29 @@ class Character extends MovableObject {
   }
 
   animate() {
-    setInterval(() => {
-      if (this.world.keyboard.RIGHT) {
 
+    setInterval(()=>{
+        if (this.world.keyboard.RIGHT) {
+            this.x += this.speed;
+        }
+
+        if (this.world.keyboard.LEFT) {
+            this.x -= this.speed;
+        }
+
+    }, 1000 / 60);
+
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+        //Walk Animation
         let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 0, Rest 0
-        // i = 0, 1, 2, 3, 4, 5, 6, 0
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
       }
-    }, 100);
+    }, 50);
   }
 
   jump() {}
