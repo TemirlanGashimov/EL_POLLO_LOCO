61f1d042a382c08efec01555b60a08a30086c9e8
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-20 11:02:01 +0100

    added  if() for the keyboard

--- models/character.class.js
+++ models/character.class.js
@@ -1,34 +1,35 @@
 class Character extends MovableObject {
+  height = 300;
+  y = 140;
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  world;
 
-    height = 300;
-    y = 140;
-    IMAGES_WALKING = [
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-22.png',
-            'img/2_character_pepe/2_walk/W-23.png',
-            'img/2_character_pepe/2_walk/W-24.png',
-            'img/2_character_pepe/2_walk/W-25.png',
-            'img/2_character_pepe/2_walk/W-26.png'
-    ];
+  constructor() {
+    super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
 
-    constructor(){
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
 
-        this.animate();
-    }
+  animate() {
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT) {
 
-    animate() {
-        setInterval(() => {
         let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 0, Rest 0
         // i = 0, 1, 2, 3, 4, 5, 6, 0
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
+      }
     }, 100);
-}
-    
-    jump(){
+  }
 
-    }
+  jump() {}
 }
