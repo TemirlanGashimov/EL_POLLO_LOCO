a6b19c18d457c62f4636cbc8f5ce399769ebca6b
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-24 10:30:11 +0100

    Added falling speed and jump height settings. Also moved the character’s walking animation logic for left and right directions.

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,37 +3,64 @@ class MovableObject {
   y = 280;
   img;
   height = 150; // höhe von objecten wie Charakter Chicken
-  width = 100;  // breite von objecten wie Character und Chicken
+  width = 100; // breite von objecten wie Character und Chicken
   imageCache = {};
   currentImage = 0;
   speed = 0.15;
   otherDirection = false;
+  0;
+  speedY = 0;
+  acceleration = 2.5;
 
-//loadImage('img/test.png);
-  loadImage(path){
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 140;
+  }
+
+  //loadImage('img/test.png);
+  loadImage(path) {
     this.img = new Image(); //this.img = document.getElementById('image') <img id="image" src>
     this.img.src = path;
   }
 
   //**
-//    * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
-//  
-
-  loadImages(arr){
+  //    * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+  //
+  loadImages(arr) {
     arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;
-        this.imageCache[path] = img;
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
     });
   }
 
-  moveRight() {
-    console.log("Moving right");
+  playAnimation(images) {
+    let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 0, Rest 0
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
   }
 
-   moveLeft(){
-        setInterval(() =>{
-            this.x -= this.speed; //bewegen sich nach links, 60 mal pro sekunde wird 0.15pixel von x coordinate von wolke abgezogen
-        }, 1000 / 60); // 60 mal pro sekunde 
-    }
+  moveRight() {
+    this.x += this.speed;
+    this.otherDirection = false;
+  }
+
+  moveLeft() {
+    
+      this.x -= this.speed; //bewegen sich nach links, 60 mal pro sekunde wird 0.15pixel von x coordinate von wolke abgezogen
+            this.otherDirection = true; // wenn ich linke taste drücke bild gespiegelt quasi Charakter dreht sich zurück 
+  }
+
+  jump() {
+    this.speedY = 30;
+  }
 }
