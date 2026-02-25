b0ca518ed27e888bd462d3e3af9d5c54b6ca3185
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-25 14:31:19 +0100

    Added bottle rotation while the character throws it.

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,19 +1,36 @@
 class ThrowableObject extends MovableObject {
-  
-    constructor(x, y) {
-    super().loadImage("img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+  IMAGES_BOTTLE = [
+    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  constructor(x, y) {
+    super().loadImage(
+      "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    );
+    this.loadImages(this.IMAGES_BOTTLE);
     this.x = x;
     this.y = y;
-    this.height = 60;
-    this.width = 60;
+    this.height = 70;
+    this.width = 70;
     this.trow();
+    this.animate();
   }
 
-  trow(){
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_BOTTLE);
+    }, 80);
+  }
+
+  trow() {
     this.speedY = 30;
     this.applyGravity();
     setInterval(() => {
-        this.x += 10;
-    }, 25);
+      //das hier nur die bewegund von flasche
+      this.x += 8; // kleinere schritte am fliegen
+    }, 16);
   }
 }
