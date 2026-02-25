20a427ae4adcf319fa414e0b68735a45afe1bf98
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-25 11:09:06 +0100

    added keyboard D for attack

--- js/game.js
+++ js/game.js
@@ -11,6 +11,7 @@ function init(){
 }
 
 window.addEventListener("keydown", (e) => {
+    
     if(e.keyCode == 39){
         keyboard.RIGHT = true;
     }
@@ -29,6 +30,10 @@ window.addEventListener("keydown", (e) => {
 
     if(e.keyCode == 32) {
         keyboard.SPACE = true;
+    }
+
+    if(e.keyCode == 68) {
+        keyboard.D = true;
     }
     
 });
@@ -52,6 +57,9 @@ window.addEventListener("keyup", (e) => {
 
     if(e.keyCode == 32) {
         keyboard.SPACE = false;
+    }
+    if(e.keyCode == 68) {
+        keyboard.D = false;
     }
     
 });
