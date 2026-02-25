f1dafbf7d03f7078bd168c2391d74a247c0722e3
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-25 11:11:41 +0100

    Added collision handling for chickens and the character.

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -18,6 +18,16 @@ draw(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
+   drawFrame(ctx){
+    if(this instanceof Character || this instanceof Chicken){ // damit übergeben wird die unten definierte code das der nur die Chicken und Character in Käschen nimmt 
+    ctx.beginPath();
+    ctx.lineWidth = '5';
+    ctx.strokeStyle = 'blue';
+    ctx.rect (this.x, this.y, this.width, this.height);
+    ctx.stroke()
+  }
+  }
+
   //**
   //    * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
   //
