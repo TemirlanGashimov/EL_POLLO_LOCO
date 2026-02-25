2dd151a5afcce5a6882a4be54190afa6855d1e4d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-25 11:53:30 +0100

    Added a bottle object and status bar. Implemented throwing bottles with the ‘D’ key, and fixed the camera so that bottles are thrown from the character when moving forward or backward.

--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,8 @@ class World {
   ctx;
   keyboard;
   camera_x = 0;
+  statusBar = new StatusBar();
+  throwableObjects = [];
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -12,21 +14,35 @@ class World {
     this.keyboard = keyboard;
     this.draw();
     this.setWorld();
-    this.checkCollisions();
+    this.run();
   }
 
   setWorld() {
     this.character.world = this;
   }
 
-  checkCollisions(){
+  run() {
     setInterval(() => {
-        this.level.enemies.forEach( (enemy) => {
-            if(this.character.isColliding(enemy) ) {
-                this.character.hit();
-            }
-        });
+
+        this.checkCollisions();
+        this.checkThrowObjects();
     }, 200);
+  }
+
+  checkThrowObjects(){
+    if(this.keyboard.D){
+        let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+        this.throwableObjects.push(bottle);
+    }
+  }
+
+  checkCollisions(){
+      this.level.enemies.forEach((enemy) => {
+        if (this.character.isColliding(enemy)) {
+          this.character.hit();
+          this.statusBar.setPercentage(this.character.energy);
+        }
+      });
   }
 
   // Draw() wird immer wieder aufgerufen
@@ -34,12 +50,17 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // canvas wird gelöscht
 
     this.ctx.translate(this.camera_x, 0); // dadurch bewegt sich die kamera nach rechts
-
     this.addObjectsToMap(this.level.backgroundObjects); // das hintergrund soll als erste dargestellt werden damit die objecte auf der hintergrund zu sehen/befinden sind
+
+    this.ctx.translate(-this.camera_x, 0);
+    // ------ Space for fixed objects ------
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0); // Forwards
 
     this.addToMap(this.character); //unsere caracter
     this.addObjectsToMap(this.level.clouds); // unsere wolken
     this.addObjectsToMap(this.level.enemies); // unsere genger, Chicken
+    this.addObjectsToMap(this.throwableObjects); // unsere flasche
 
     this.ctx.translate(-this.camera_x, 0); // dadurch unsere kamear bewegt sich nach links
 
@@ -65,20 +86,19 @@ class World {
     if (mo.otherDirection) {
       this.flipImageBack(mo);
     }
-}
+  }
 
-flipImage(mo){
+  flipImage(mo) {
     // wir schauen ob unsere objekt eine andere richtung hat
-      this.ctx.save(); // wenn ja speichern wir aktuelle  einstellung unsere context/bilder
-      this.ctx.translate(mo.width, 0); // dann verändern wir die methode wie wir die bilder einfügen
-      this.ctx.scale(-1, 1); // und drehen alle in y Achse wir spiegeln einmal  alles
-      mo.x = mo.x * -1;
-}
+    this.ctx.save(); // wenn ja speichern wir aktuelle  einstellung unsere context/bilder
+    this.ctx.translate(mo.width, 0); // dann verändern wir die methode wie wir die bilder einfügen
+    this.ctx.scale(-1, 1); // und drehen alle in y Achse wir spiegeln einmal  alles
+    mo.x = mo.x * -1;
+  }
 
-flipImageBack(mo){
-// wenn unsere context sich verändert habe wie wir in oberen if statement sehen hier mache wir das alles wieder rückganging
-      mo.x = mo.x * -1;
-      this.ctx.restore();
+  flipImageBack(mo) {
+    // wenn unsere context sich verändert habe wie wir in oberen if statement sehen hier mache wir das alles wieder rückganging
+    mo.x = mo.x * -1;
+    this.ctx.restore();
+  }
 }
-}
-
