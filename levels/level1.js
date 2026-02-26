5911455df84d83e6de2e1edc192ff42dd9b3a446
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-26 14:04:14 +0100

    added some more chicken and added coin

--- levels/level1.js
+++ levels/level1.js
@@ -3,10 +3,23 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
         new Endboss()
     ],
     [
         new Cloud()
+    ],
+
+    [
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin()
+
     ],
     [
         new BackgroundObject('img/5_background/layers/air.png', -720),
