0aa69a3f1756b88ed5c7b393fde0b693e30fc8c4
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-26 14:09:19 +0100

    Added coins, including offset adjustments, coin animation, and random coin placement.

---
+++ models/coin.class.js
@@ -0,0 +1,26 @@
+class Coin extends MovableObject {
+  offset = {
+    top: 52,
+    left: 33,
+    right: 33,
+    bottom: 52,
+  };
+
+  IMAGES_COIN = [
+    'img/8_coin/coin_1.png', 
+    'img/8_coin/coin_2.png'];
+
+  constructor() {
+    super().loadImage('img/8_coin/coin_1.png');
+    this.loadImages(this.IMAGES_COIN);
+
+    this.x = 200 + Math.random() * 500 * 3;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_COIN);
+    }, 200);
+  }
+}
