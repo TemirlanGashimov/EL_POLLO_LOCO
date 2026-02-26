63bd7ca39e12b10ac8ec7aac976fb95778906bdf
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-02-26 14:10:10 +0100

    Added a global offset with all values set to 0 and made changes in drawFrame.

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -2,30 +2,45 @@ class DrawableObject {
   img;
   imageCache = {};
   currentImage = 0;
+
   x = 120;
   y = 280;
   height = 150; // höhe von objecten wie Charakter Chicken
   width = 100; // breite von objecten wie Character und Chicken
 
+  offset = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
 
-//loadImage('img/test.png);
+  //loadImage('img/test.png);
   loadImage(path) {
     this.img = new Image(); //this.img = document.getElementById('image') <img id="image" src>
     this.img.src = path;
   }
 
-draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
-   drawFrame(ctx){
-    if(this instanceof Character || this instanceof Chicken){ // damit übergeben wird die unten definierte code das der nur die Chicken und Character in Käschen nimmt 
-    ctx.beginPath();
-    ctx.lineWidth = '5';
-    ctx.strokeStyle = 'blue';
-    ctx.rect (this.x, this.y, this.width, this.height);
-    ctx.stroke()
-  }
+  drawFrame(ctx) {
+    if (this instanceof Character ||this instanceof Chicken ||this instanceof Endboss ||this instanceof Coin) {
+     
+        // damit übergeben wird die unten definierte code dass der die Chicken Character coins und Endboss in Käschen nimmt
+      ctx.beginPath();
+      ctx.lineWidth = "2";
+      ctx.strokeStyle = "red";
+
+      ctx.rect(
+        this.x + this.offset.left, 
+        this.y + this.offset.top,
+        this.width - this.offset.left - this.offset.right, 
+        this.height - this.offset.top - this.offset.bottom
+    );
+      ctx.stroke();
+    }
   }
 
   //**
@@ -38,17 +53,4 @@ draw(ctx) {
       this.imageCache[path] = img;
     });
   }
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
