7a1501b1b71fd68a3613cf10744801573f7d589d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-02 14:27:00 +0100

    Fix width value in StatusBarHealth class constructor

--- models/statusbar-health.class.js
+++ models/statusbar-health.class.js
@@ -15,7 +15,7 @@ class StatusBarHealth extends DrawableObject {
     this.loadImages(this.IMAGES);
     this.x = 20;
     this.y = 0;
-    this.width = 250;
+    this.width = 200;
     this.height = 60;
     this.setPercentage(100);
   }
