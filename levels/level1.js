92d485f20ada341a6121f090004ffc1ad2064a2e
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-02 12:08:08 +0100

    Refactor level1.js: streamline object initialization and enhance background layering

--- levels/level1.js
+++ levels/level1.js
@@ -1,64 +1,84 @@
 const level1 = new Level(
-    [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Endboss()
-    ],
-    [
-        new Cloud()
-    ],
+  [
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Endboss(),
+  ],
+  [new Cloud()],
 
-    [
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
+  [
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin(),
+    new Coin()
+  ],
 
-    ],
+  [
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle(),
+    new Bottle()
+  ],
+  [
+    new BackgroundObject("img/5_background/layers/air.png", -720),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -720),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -720),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -720),
 
-    [
-        new Bottle(),
-        new Bottle(),
-        new Bottle(),
-        new Bottle(),
-        new Bottle(),
-        new Bottle(),
-        new Bottle(),
-        new Bottle()
-    ],
+    new BackgroundObject("img/5_background/layers/air.png", 0),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0), // 0,100 positionierung von hintergrundBild
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/air.png", 720),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 720),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 720),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 720),
 
-    [
-        new BackgroundObject('img/5_background/layers/air.png', -720),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -720), 
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -720),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -720),
-
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0), // 0,100 positionierung von hintergrundBild
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/air.png', 720),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720), 
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720),
-
-        new BackgroundObject('img/5_background/layers/air.png', 720*2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720*2), 
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720*2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720*2),
-        new BackgroundObject('img/5_background/layers/air.png', 720*3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720*3), 
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720*3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720*3)
-    ]
+    new BackgroundObject("img/5_background/layers/air.png", 720 * 2),
+    new BackgroundObject(
+      "img/5_background/layers/3_third_layer/1.png",
+      720 * 2,
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/2_second_layer/1.png",
+      720 * 2,
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/1_first_layer/1.png",
+      720 * 2,
+    ),
+    new BackgroundObject("img/5_background/layers/air.png", 720 * 3),
+    new BackgroundObject(
+      "img/5_background/layers/3_third_layer/2.png",
+      720 * 3,
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/2_second_layer/2.png",
+      720 * 3,
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/1_first_layer/2.png",
+      720 * 3,
+    ),
+  ],
 );
