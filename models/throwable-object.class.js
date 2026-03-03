b343a4834859b18fd54100829fdd67a0a3a32bda
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-03 15:24:44 +0100

    Add bottle break functionality: implement isBroken state, sound effect, and modify throw logic

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,10 +1,17 @@
 class ThrowableObject extends MovableObject {
+
+isBroken = false;
+throwInterval;
+
+
   IMAGES_BOTTLE = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
   ];
+
+  bottleBreak_sound = new Audio("sounds/throwable/bottleBreak.mp3");
 
   constructor(x, y) {
     super().loadImage(
@@ -28,9 +35,30 @@ class ThrowableObject extends MovableObject {
   throw() {
     this.speedY = 30;
     this.applyGravity();
-    setInterval(() => {
+    this.throwInterval = setInterval(() => {
       //das hier nur die bewegund von flasche
-      this.x += 8; // kleinere schritte am fliegen
+      if(!this.isBroken){
+        this.x += 8; // kleinere schritte am fliegen
+      
+        if (this.y >= 140) {
+          this.y = 140;
+          this.break();
+        }
+      }
     }, 16);
   }
+
+
+break() {
+  if (this.isBroken) return;
+
+  this.isBroken = true;
+
+  clearInterval(this.throwInterval);
+
+  this.speedY = 0;
+
+  this.bottleBreak_sound.currentTime = 0;
+  this.bottleBreak_sound.play();
+}
 }
