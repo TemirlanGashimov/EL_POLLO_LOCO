4c0f8cf93c82d6d1952fc47074be6cb0e307d63d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-04 12:17:00 +0100

    Add image arrays for end boss: define IMAGES_WALK, IMAGES_ATTACK, IMAGES_HURT, and IMAGES_DEAD

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -12,6 +12,14 @@ class Endboss extends MovableObject {
         bottom: 25
     };
 
+    IMAGES_WALK = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png'
+
+    ];
+
     IMAGES_WALKING = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
@@ -20,13 +28,41 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G9.png',
         'img/4_enemie_boss_chicken/2_alert/G10.png',
         'img/4_enemie_boss_chicken/2_alert/G11.png',
-        'img/4_enemie_boss_chicken/2_alert/G12.png',
+        'img/4_enemie_boss_chicken/2_alert/G12.png'
 
+    ];
+
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png'
+
+    ];
+
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/3_attack/G21.png',
+        'img/4_enemie_boss_chicken/3_attack/G22.png',
+        'img/4_enemie_boss_chicken/3_attack/G23.png'
+    ];
+
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/3_attack/G24.png',
+        'img/4_enemie_boss_chicken/3_attack/G25.png',
+        'img/4_enemie_boss_chicken/3_attack/G26.png'
     ];
 
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALK);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 840*3;
         this.animate();
     }
