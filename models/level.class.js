ebfe9e6ea5ef411a7569008154b6e19f48c8ba6b
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-04 14:13:29 +0100

    No changes were made.

--- models/level.class.js
+++ models/level.class.js
@@ -1,18 +1,17 @@
 class Level {
-    enemies;
-    clouds;
-    coins;
-    bottles;
-    backgroundObjects;
-    level_end_x = 740*3;
+  enemies;
+  clouds;
+  coins;
+  bottles;
+  backgroundObjects;
+  level_end_x = 740 * 3;
 
-    constructor(enemies,clouds,coins,bottles,backgroundObjects){
-        this.enemies = enemies;
-        this.clouds = clouds;
-        this.coins = coins;
-        this.bottles = bottles;
-        this.backgroundObjects = backgroundObjects;
-       
-        
-    }
+  
+  constructor(enemies, clouds, coins, bottles, backgroundObjects) {
+    this.enemies = enemies;
+    this.clouds = clouds;
+    this.coins = coins;
+    this.bottles = bottles;
+    this.backgroundObjects = backgroundObjects;
+  }
 }
