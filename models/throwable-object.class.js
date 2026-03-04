78901c96610f532e2a728727b3b1cfc1a1293522
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-04 14:26:08 +0100

    added spalsh images

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,14 +1,21 @@
 class ThrowableObject extends MovableObject {
-
-isBroken = false;
-throwInterval;
-
+  isBroken = false;
+  throwInterval;
 
   IMAGES_BOTTLE = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  SPLASH_IMAGES = [
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
   bottleBreak_sound = new Audio("sounds/throwable/bottleBreak.mp3");
@@ -37,9 +44,9 @@ throwInterval;
     this.applyGravity();
     this.throwInterval = setInterval(() => {
       //das hier nur die bewegund von flasche
-      if(!this.isBroken){
+      if (!this.isBroken) {
         this.x += 8; // kleinere schritte am fliegen
-      
+
         if (this.y >= 140) {
           this.y = 140;
           this.break();
@@ -48,17 +55,16 @@ throwInterval;
     }, 16);
   }
 
+  break() {
+    if (this.isBroken) return;
 
-break() {
-  if (this.isBroken) return;
+    this.isBroken = true;
 
-  this.isBroken = true;
+    clearInterval(this.throwInterval);
 
-  clearInterval(this.throwInterval);
+    this.speedY = 0;
 
-  this.speedY = 0;
-
-  this.bottleBreak_sound.currentTime = 0;
-  this.bottleBreak_sound.play();
-}
+    this.bottleBreak_sound.currentTime = 0;
+    this.bottleBreak_sound.play();
+  }
 }
