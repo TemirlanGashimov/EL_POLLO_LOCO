69caf570385e6bfd8bb02a1aa73b2b0515e780c9
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-04 14:12:56 +0100

    changed the path for the images and introduced variables for better flexibility. Additionally, we implemented animations for attacking and walking. When the boss receives damage, a hit animation is triggered and the boss's health decreases accordingly.

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,107 +1,149 @@
 class Endboss extends MovableObject {
+  height = 400;
+  width = 250;
+  y = 50;
+  energy = 100;
+  isDead = false;
+  isHurt = false;
+  isAttacking = false;
+  lastAttack = 0;
+  lastHit = 0;
 
-    height = 400;
-    width = 250;
-    y = 50;
-    energy = 100;
-    isDead = false;
-    isHurt = false;
+  offset = {
+    top: 70,
+    left: 30,
+    right: 30,
+    bottom: 25,
+  };
 
-     offset = {
-        top: 70,
-        left: 30,
-        right: 30,
-        bottom: 25
-    };
+  IMAGES_WALK = [
+    "img/4_enemie_boss_chicken/1_walk/G1.png",
+    "img/4_enemie_boss_chicken/1_walk/G2.png",
+    "img/4_enemie_boss_chicken/1_walk/G3.png",
+    "img/4_enemie_boss_chicken/1_walk/G4.png",
+  ];
 
-    IMAGES_WALK = [
-        'img/4_enemie_boss_chicken/1_walk/G1.png',
-        'img/4_enemie_boss_chicken/1_walk/G2.png',
-        'img/4_enemie_boss_chicken/1_walk/G3.png',
-        'img/4_enemie_boss_chicken/1_walk/G4.png'
+  IMAGES_WALKING = [
+    "img/4_enemie_boss_chicken/2_alert/G5.png",
+    "img/4_enemie_boss_chicken/2_alert/G6.png",
+    "img/4_enemie_boss_chicken/2_alert/G7.png",
+    "img/4_enemie_boss_chicken/2_alert/G8.png",
+    "img/4_enemie_boss_chicken/2_alert/G9.png",
+    "img/4_enemie_boss_chicken/2_alert/G10.png",
+    "img/4_enemie_boss_chicken/2_alert/G11.png",
+    "img/4_enemie_boss_chicken/2_alert/G12.png",
+  ];
 
-    ];
+  IMAGES_ATTACK = [
+    "img/4_enemie_boss_chicken/3_attack/G13.png",
+    "img/4_enemie_boss_chicken/3_attack/G14.png",
+    "img/4_enemie_boss_chicken/3_attack/G15.png",
+    "img/4_enemie_boss_chicken/3_attack/G16.png",
+    "img/4_enemie_boss_chicken/3_attack/G17.png",
+    "img/4_enemie_boss_chicken/3_attack/G18.png",
+    "img/4_enemie_boss_chicken/3_attack/G19.png",
+    "img/4_enemie_boss_chicken/3_attack/G20.png",
+  ];
 
-    IMAGES_WALKING = [
-        'img/4_enemie_boss_chicken/2_alert/G5.png',
-        'img/4_enemie_boss_chicken/2_alert/G6.png',
-        'img/4_enemie_boss_chicken/2_alert/G7.png',
-        'img/4_enemie_boss_chicken/2_alert/G8.png',
-        'img/4_enemie_boss_chicken/2_alert/G9.png',
-        'img/4_enemie_boss_chicken/2_alert/G10.png',
-        'img/4_enemie_boss_chicken/2_alert/G11.png',
-        'img/4_enemie_boss_chicken/2_alert/G12.png'
+  IMAGES_HURT = [
+    "img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "img/4_enemie_boss_chicken/4_hurt/G23.png",
+  ];
 
-    ];
+  IMAGES_DEAD = [
+    "img/4_enemie_boss_chicken/5_dead/G24.png",
+    "img/4_enemie_boss_chicken/5_dead/G25.png",
+    "img/4_enemie_boss_chicken/5_dead/G26.png",
+  ];
 
-    IMAGES_ATTACK = [
-        'img/4_enemie_boss_chicken/3_attack/G13.png',
-        'img/4_enemie_boss_chicken/3_attack/G14.png',
-        'img/4_enemie_boss_chicken/3_attack/G15.png',
-        'img/4_enemie_boss_chicken/3_attack/G16.png',
-        'img/4_enemie_boss_chicken/3_attack/G17.png',
-        'img/4_enemie_boss_chicken/3_attack/G18.png',
-        'img/4_enemie_boss_chicken/3_attack/G19.png',
-        'img/4_enemie_boss_chicken/3_attack/G20.png'
+  constructor() {
+    super().loadImage(this.IMAGES_WALK[0]);
+    this.loadImages(this.IMAGES_WALK);
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_ATTACK);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_DEAD);
+    this.x = 840 * 3;
+    this.animate();
+  }
 
-    ];
+  animate() {
+    setInterval(() => {
+      if (this.isDead) {
+        this.playAnimation(this.IMAGES_DEAD);
+      } else if (this.isHurt) {
+        this.playAnimation(this.IMAGES_HURT);
+      } else if (this.isAttacking) {
+        this.playAnimation(this.IMAGES_ATTACK);
+      } else if (this.isPlayerNear()) {
+        this.playAnimation(this.IMAGES_WALK);
+      } else {
+        this.playAnimation(this.IMAGES_WALKING);
+      }
+    }, 200);
 
-    IMAGES_HURT = [
-        'img/4_enemie_boss_chicken/3_attack/G21.png',
-        'img/4_enemie_boss_chicken/3_attack/G22.png',
-        'img/4_enemie_boss_chicken/3_attack/G23.png'
-    ];
+    setInterval(() => {
+      if (
+        this.isPlayerVeryNear() &&
+        !this.isDead &&
+        !this.isAttacking &&
+        !this.isHurt
+      ) {
+        this.attack();
+      }
 
-    IMAGES_DEAD = [
-        'img/4_enemie_boss_chicken/3_attack/G24.png',
-        'img/4_enemie_boss_chicken/3_attack/G25.png',
-        'img/4_enemie_boss_chicken/3_attack/G26.png'
-    ];
+      if (
+        this.isPlayerNear() &&
+        !this.isDead &&
+        !this.isAttacking &&
+        !this.isHurt
+      ) {
+        this.x -= 1;
+      }
+    }, 1000 / 60);
+  }
 
-    constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALK);
-        this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_ATTACK);
-        this.loadImages(this.IMAGES_HURT);
-        this.loadImages(this.IMAGES_DEAD);
-        this.x = 840*3;
-        this.animate();
+  hit() {
+    let now = new Date().getTime();
+
+    if (now - this.lastHit < 500) return;
+
+    this.lastHit = now;
+
+    if (this.energy > 0) {
+      this.energy -= 20;
+      this.isHurt = true;
+
+      setTimeout(() => {
+        this.isHurt = false;
+      }, 400);
     }
 
-    animate(){
-        setInterval(() => {
-            if (this.isDead){
-                this.playAnimation(this.IMAGES_DEAD);
-            } else if (this.isHurt) {
-                this.playAnimation(this.IMAGES_HURT);
-            }else  {
-            this.playAnimation(this.IMAGES_WALKING);
-            }
-        }, 200);
-        
-        setInterval(() => {
-        this.x -2;
-    }, 1000/60)
+    if (this.energy <= 0) {
+      this.energy = 0;
+      this.isDead = true;
     }
+  }
 
-    
+  attack() {
+    let now = new Date().getTime();
 
-    hit() {
-        if(this.energy > 0){
-        this.energy -= 20;
-        this.isHurt = true;
+    if (now - this.lastAttack > 1500) {
+      this.isAttacking = true;
+      this.lastAttack = now;
 
-        setTimeout(() => {
-            this.isHurt = false;
-        }, 400);
-        }
-
-        if(this.energy <= 0) {
-            this.energy = 0;
-            this.isDead = true;
-        }
+      setTimeout(() => {
+        this.isAttacking = false;
+      }, 800);
     }
+  }
 
+  isPlayerNear() {
+    return Math.abs(this.x - this.world.character.x) < 600;
+  }
 
+  isPlayerVeryNear() {
+    return Math.abs(this.x - this.world.character.x) < 200;
+  }
 }
