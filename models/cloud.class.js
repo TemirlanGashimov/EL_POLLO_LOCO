f4873bba3a67aa60bb215754b4abeeb2ca7c5c88
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-16 13:46:37 +0100

    added new cloud

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,20 +1,35 @@
 class Cloud extends MovableObject {
-y = 20;
-width = 500;
-height = 250;
 
+  y = 20;
+  width = 500;
+  height = 250;
 
-    constructor(){
-        super().loadImage('img/5_background/layers/4_clouds/1.png');
-    
-    this.x = Math.random() * 500 * 3; //Zahl zwischen 200 und 700 
+  CLOUD_IMAGES = [
+    "img/5_background/layers/4_clouds/1.png",
+    "img/5_background/layers/4_clouds/2.png"
+  ];
+
+  constructor() {
+
+    let cloudImages = [
+      "img/5_background/layers/4_clouds/1.png",
+      "img/5_background/layers/4_clouds/2.png"
+    ];
+
+    let randomIndex = Math.floor(Math.random() * cloudImages.length);
+
+    super().loadImage(cloudImages[randomIndex]);
+
+    this.x = Math.random() * 500 * 3;
+
     this.animate();
-    }
+  }
 
-    animate(){  //Wir animieren die Wolken damit di sich bewegen 
-        this.moveLeft();
-    }
+  animate(){
+    this.speed = 0.15;
 
-   
-
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+  }
 }
