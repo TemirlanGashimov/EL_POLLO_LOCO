fbe2b66d76905b7c853c4a35a1a0465166138f62
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-31 18:51:47 +0200

    added comments

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,22 @@
+/**
+ * Represents a background object in the game.
+ * Extends the MovableObject class.
+ * @extends MovableObject
+ */
 class BackgroundObject extends MovableObject{
 
+    /** @type {number} Fixed width of the background object */
     width = 720; //feste breite für hintergrund
+
+    /** @type {number} Fixed height of the background object */
     height = 480; // feste höhe für hintergrund
+
+    /**
+     * Creates a new BackgroundObject instance.
+     * @param {string} imagePath - Path to the background image.
+     * @param {number} x - X position of the background object.
+     * @param {number} y - Y position (not directly used, since it is calculated).
+     */
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
         this.x = x;
