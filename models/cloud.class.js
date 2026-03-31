6fc078064ef67345fca1f391ce0665ab07fdc567
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-31 18:52:28 +0200

    fixed code

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,34 +1,31 @@
 class Cloud extends MovableObject {
-
   y = 20;
   width = 500;
   height = 250;
 
   CLOUD_IMAGES = [
     "img/5_background/layers/4_clouds/1.png",
-    "img/5_background/layers/4_clouds/2.png"
+    "img/5_background/layers/4_clouds/2.png",
   ];
 
   constructor() {
+    super(); // 🔥 MUSS als erstes kommen
 
-    let cloudImages = [
-      "img/5_background/layers/4_clouds/1.png",
-      "img/5_background/layers/4_clouds/2.png"
-    ];
+    let randomIndex = Math.floor(Math.random() * this.CLOUD_IMAGES.length);
+    this.loadImage(this.CLOUD_IMAGES[randomIndex]);
 
-    let randomIndex = Math.floor(Math.random() * cloudImages.length);
-
-    super().loadImage(cloudImages[randomIndex]);
-
-    this.x = Math.random() * 500 * 3;
-
+    this.x = Math.random() * 1500;
     this.animate();
   }
 
-  animate(){
+  animate() {
     this.speed = 0.15;
+    this.handleMovement();
+  }
 
+  handleMovement() {
     setInterval(() => {
+      if (!this.world?.gameRunning || this.world.gameOver) return;
       this.moveLeft();
     }, 1000 / 60);
   }
