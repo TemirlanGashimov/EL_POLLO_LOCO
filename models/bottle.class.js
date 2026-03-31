74155707c68a001ab175ba3cb27ae45852563427
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-03-31 18:51:58 +0200

    added comments

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,7 +1,20 @@
+/**
+ * Represents a bottle object in the game.
+ * Extends the MovableObject class.
+ * @extends MovableObject
+ */
 class Bottle extends MovableObject {
+
+  /** @type {number} Height of the bottle */
   height = 80;
+
+  /** @type {number} Vertical position of the bottle */
   y = 350;
 
+  /**
+   * Collision offset values for more accurate hit detection.
+   * @type {{top: number, left: number, right: number, bottom: number}}
+   */
   offset = {
     top: 15,
     left: 45,
@@ -9,10 +22,18 @@ class Bottle extends MovableObject {
     bottom: 10,
   };
 
+  /**
+   * Array of image paths representing the bottle.
+   * @type {string[]}
+   */
   IMAGES_BOTTLE = [
     "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
   ];
 
+  /**
+   * Creates a new Bottle instance.
+   * Initializes position and loads images.
+   */
   constructor() {
     super().loadImage("img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
     this.loadImages(this.IMAGES_BOTTLE);
